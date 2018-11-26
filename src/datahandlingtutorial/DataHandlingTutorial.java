a808536cba5b2a867f4a4451f8a9893f5c2ecf19
Samson Daniel <44621365+SamsonCodes@users.noreply.github.com>
2018-11-26 10:14:27 +0100

    Initial commit

---
+++ src/datahandlingtutorial/DataHandlingTutorial.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datahandlingtutorial;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DataHandlingTutorial 
+{
+    public final static String PATH = "C:\\Users\\Samson\\Documents\\NetBeansProjects\\DataHandlingTutorial\\src\\";
+    
+    public static void saveData(ArrayList<String> data, String filePath)
+    {
+        try
+        {
+            PrintWriter writer = new PrintWriter(filePath, "UTF-8");
+            for(String line: data)
+            {
+                writer.println(line);
+            }
+            writer.close();
+        }
+        catch (FileNotFoundException | UnsupportedEncodingException ex)
+        {
+            Logger.getLogger(DataHandlingTutorial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+       ArrayList<String> data = new ArrayList();
+       data.add("test123");
+       saveData(data, PATH + "test.txt");
+    }
+
+}
